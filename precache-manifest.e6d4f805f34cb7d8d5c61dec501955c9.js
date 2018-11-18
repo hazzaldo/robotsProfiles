391b30bb449e8ff5a1ceac2f6555d3ef9a5444b6
hazzaldo <hazzaldo@hotmail.com>
2018-11-18 18:33:03 +0000

    Updates

---
+++ precache-manifest.e6d4f805f34cb7d8d5c61dec501955c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotsProfiles/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "67a01b71b07af0dfdb64",
+    "url": "/robotsProfiles/static/js/runtime~main.67a01b71.js"
+  },
+  {
+    "revision": "f7c5b304fd5c24aecfde",
+    "url": "/robotsProfiles/static/js/main.f7c5b304.chunk.js"
+  },
+  {
+    "revision": "ea5ef8accb846295146e",
+    "url": "/robotsProfiles/static/js/1.ea5ef8ac.chunk.js"
+  },
+  {
+    "revision": "f7c5b304fd5c24aecfde",
+    "url": "/robotsProfiles/static/css/main.ea39c474.chunk.css"
+  },
+  {
+    "revision": "ea5ef8accb846295146e",
+    "url": "/robotsProfiles/static/css/1.6d609ce6.chunk.css"
+  },
+  {
+    "revision": "99828e24e0b81db692af08379d25843b",
+    "url": "/robotsProfiles/index.html"
+  }
+];
